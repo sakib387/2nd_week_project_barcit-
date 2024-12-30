9747c7f4623ba05245312932d008422334bfd711
Sakib Mollah <sakib.mollah@bracits.com>
2024-12-30 22:12:12 +0600

    crud opereaion done

--- LibraryInventory/src/main/java/com/example/LibraryInventory/controller/LibraryController.java
+++ LibraryInventory/src/main/java/com/example/LibraryInventory/controller/LibraryController.java
@@ -1,32 +1,40 @@
 package com.example.LibraryInventory.controller;
+import com.example.LibraryInventory.model.Book;
+import com.example.LibraryInventory.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 public class LibraryController {
 
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("/")
-    public String displayAllBooks(){
-        return "all book";
+    public List<Book> displayAllBooks(){
+        return bookService.getAllBooks();
     }
 
-    @GetMapping("/book/{bookId}")
-    public String sow(){
-        return "book";
+    @GetMapping("/book/{key}")
+    public List<Book> findBooks(@PathVariable String key){
+        return bookService.findBooks(key);
     }
 
     @PutMapping("/update/{bookId}")
-    public String updateBook(){
-        return "book update";
+    public String updateBook(@PathVariable String bookId,@RequestBody Book book){
+        return bookService.updateBooks(bookId,book);
     }
 
     @PostMapping("/add")
-    public String addBook(){
-        return "book add";
+    public String addBook(@RequestBody Book book){
+        return bookService.addBook(book);
     }
 
     @DeleteMapping("/delete/{bookId}")
-    public String deleteBook(){
-        return "book delete";
+    public String deleteBook(@PathVariable String bookId){
+        return bookService.deleteBook(bookId);
     }
 }
--- LibraryInventory/src/main/java/com/example/LibraryInventory/repository/BookRepository.java
+++ LibraryInventory/src/main/java/com/example/LibraryInventory/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.example.LibraryInventory.repository;
 
+import com.example.LibraryInventory.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class BookRepository {
+
 }
