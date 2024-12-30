f6f9fd0b79304577c2167a390118840f129cc331
Sakib Mollah <sakib.mollah@bracits.com>
2024-12-30 16:22:01 +0600

    project set

---
+++ LibraryInventory/src/main/java/com/example/LibraryInventory/LibraryInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryInventoryApplication.class, args);
+	}
+
+}
---
+++ LibraryInventory/src/test/java/com/example/LibraryInventory/LibraryInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
