931821fa081823f8879672a29def83e2d3b2c927
Sakib Mollah <sakib.mollah@bracits.com>
2025-01-01 15:54:09 +0600

    global exceptoin handler

--- LibraryInventory/src/main/java/com/example/LibraryInventory/error/BookNotFoundException.java
+++ LibraryInventory/src/main/java/com/example/LibraryInventory/error/BookNotFoundException.java
@@ -1,5 +1,8 @@
 package com.example.LibraryInventory.error;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException{
     public BookNotFoundException(String message) {
         super(message);
---
+++ LibraryInventory/src/main/java/com/example/LibraryInventory/globalExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.example.LibraryInventory.globalExceptionHandler;
+
+import com.example.LibraryInventory.error.AlreadeyExistException;
+import com.example.LibraryInventory.error.BookNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AlreadeyExistException.class)
+    public ResponseEntity<Object> handleAlreadyExistException(AlreadeyExistException ex) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<Object> handleBookNotFoundException(BookNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message", ex.getMessage());
+
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
